f515e4bc1065c483c08b175c8238d8883c6741b1
stevensillsii <stevensills2@gmail.com>
2023-11-18 19:45:25 -0800

    node setup with initial dependencies

---
+++ index.js
@@ -0,0 +1,10 @@
+steven => {
+    console.log('Hello, I am Steven');
+    console.log('I am a software engineer');
+};
+
+function steven2() {
+    console.log('Hello, I am Steven');
+    console.log('I am a software engineer yet again');
+};
+
